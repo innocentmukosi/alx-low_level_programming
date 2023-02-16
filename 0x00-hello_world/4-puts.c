d73cbf5332df8380fed952379dadb16294755f9d
innocent mukosi <n.i.mukosi1@gmail.com>
2023-02-16 14:54:53 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *  main - entry
- *  return: 0
+ *  main - printing a line with puts function
+ *  Return: 0 (if it returns 0 it means it is successful)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	Return (0);
 }
 
