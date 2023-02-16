376cdfde78287b3ed1a880ff8d23b472b98defea
innocent mukosi <n.i.mukosi1@gmail.com>
2023-02-16 15:48:05 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - program that allows printf function
- * Return: 0 (if 0 return the code successful)
+ * Return: 0 (successful)
  */
  int main(void)
- {
+{
  printf("with proper grammar, but the outcome is a piece of art,\n")
  return (0);
- }
+}
