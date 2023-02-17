6de6979615f2f85c41189472ecb8af9f6fbbddba
innocent mukosi <n.i.mukosi1@gmail.com>
2023-02-16 20:42:41 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
 * main - program that print a line standard error
-* Return 1 (success)
+* Return: 1 (success)
 */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
