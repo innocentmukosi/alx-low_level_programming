7097e7763d4bc3f6ba4c7f08cc2f8219d657d0eb
innocent mukosi <n.i.mukosi1@gmail.com>
2023-02-16 16:33:25 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
  * main - program that allows printf function
  * Return: 0 (successful)
  */
- int main(void)
+
+int main(void)
 {
  printf("with proper grammar, but the outcome is a piece of art,\n");
  return (0);
