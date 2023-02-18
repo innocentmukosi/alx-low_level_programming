680651ddfa51dd646c3018337dc5a2ed468d798d
innocent mukosi <n.i.mukosi1@gmail.com>
2023-02-18 15:05:57 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 		if (n > 0)
-			printf("%d is positive\n", n);
+			printf("last digit of %d is positive\n", n);
 		else if (n == 0)
-			printf("%d is zero\n", n);
+			printf("last digit of %d is zero\n", n);
 		else
-			printf("%d is negetive\n", n);
+			printf("last digit of %d is negetive\n", n);
 		return (0);
 }
