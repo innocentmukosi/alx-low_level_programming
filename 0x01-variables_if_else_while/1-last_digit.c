730f8cb029c3281192422d1bff53e85a2deca3c7
innocent mukosi <n.i.mukosi1@gmail.com>
2023-02-19 15:14:09 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,15 +19,15 @@ int main(void)
 
 	if (lastdgt > 5)
 	{
-		printf("lastdgt of %d is %d and is greater than 5\n", n, lastdgt);
+		printf("lastdgt of '%d' is %d and is greater than 5\n", n, lastdgt);
 	}
 	else if (lastdgt == 0)
 	{
-		printf("lastdgt of %d is %d and is 0\n" n, lastdgt);
+		printf("lastdgt of '%d' is %d and is 0\n" n, lastdgt);
 	}
 	else if (lastdgt < 6)
 	{
-		printf("lastdgt of %d is %d and is less than 6 and not 0\n", n, lastdgt);
+		printf("lastdgt of '%d' is %d and is less than 6 and not 0\n", n, lastdgt);
 	}
 	return (0);
 }
