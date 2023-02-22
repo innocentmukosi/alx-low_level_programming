fdcab0bfcfc4df834a5660c40295e7632205d746
innocent mukosi <n.i.mukosi1@gmail.com>
2023-02-22 06:12:30 -0800

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "stdio.h"
+
+/**
+ * print_to_98 print natural numbers from n to 98
+ * Return: Always 0.
+ */
+
+void print_to_98(int n)
+{
+
+if (n <= 98)
+}
+for (; n <= 98; n++)
+{
+if (n == 98)
+{
+printf("%d", n);
+printf("/n");
+break;
+}
+else
+{
+}
+}
+else
+{
+for (; n >= 98; n--)
+{
+if (n == 98)
+{
+break;
+}
+else
+{
+printf("%d, ", n);
+}
+}
+}
+
+
