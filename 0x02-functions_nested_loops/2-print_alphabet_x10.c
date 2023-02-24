b5c07910614159b59f63134a9a7e33b5ecdba0ef
innocent mukosi <n.i.mukosi1@gmail.com>
2023-02-24 07:10:33 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,15 +10,15 @@ void print_alphabet_x10(void)
 	int i;
 
 	i = 0;
-	 while (i < 10)
-	{
-	       ch = 'a';
-       while (ch <= 'z')
-       {
+while (i < 10)
+{
+ch = 'a';
+while (ch <= 'z')
+{
 _putchar(ch);
-ch++;
-       }
-       _putchar('\n');
-       i++;
+c++;
+}
+_putchar('\n');
+i++;
 }
 }
