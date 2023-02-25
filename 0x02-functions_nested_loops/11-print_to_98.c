2a1e3273a00bbccf4cb992e4ba13d33853253efd
innocent mukosi <n.i.mukosi1@gmail.com>
2023-02-25 02:37:45 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,25 +1,28 @@
-#include "stdio.h"
+#include <stdio.h>
+#include "main.h"
 
 /**
- * print_to_98 print natural numbers from n to 98
- * Return: Always 0.
- */
+* print_to_98 Prints all natural numbers from n to 98
+* @n: the number to star printing from
+* Return: 0 (success)
+*/
 
 void print_to_98(int n)
 {
-
 if (n <= 98)
 }
 for (; n <= 98; n++)
 {
 if (n == 98)
-{
+i{
 printf("%d", n);
-printf("/n");
+printf("\n");
 break;
 }
 else
 {
+	printf("%d, ", n);
+}
 }
 }
 else
@@ -28,6 +31,8 @@ for (; n >= 98; n--)
 {
 if (n == 98)
 {
+printf("%d", n);
+printf("\n");
 break;
 }
 else
@@ -36,5 +41,5 @@ printf("%d, ", n);
 }
 }
 }
-
+}
 
