076c3ef05b0d15ac17f0400fd88fa1205019b46f
innocent mukosi <n.i.mukosi1@gmail.com>
2023-02-25 01:56:25 -0800

    11-print_to_98.c

--- 11-print_to_98.c
+++ 11-print_to_98.c
@@ -8,8 +8,7 @@
  */
 void print_to_98(int n)
 {
-	int i;
-	int j;
+	int i,j;
 
 		if : (n <= 98)
 		{
@@ -21,8 +20,8 @@ void print_to_98(int n)
 		} esle if : (n >= 98)
 }
 for (j = n; j > = 98; j--)
-{
-	if (j != 98)
+
+{if (j != 98)
 		printf("%d, ", j);
 	else if (j == 98)
 		printf("%d\n", j);
