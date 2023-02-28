3ffb07cb0f187ab57e920d61a12f752e165db9d8
innocent mukosi <n.i.mukosi1@gmail.com>
2023-02-28 11:24:14 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,17 +7,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-int 1 = 0;
-int x = 0;
-
-while (*(src + 1) != '\0')
+int i;
+for (i = 0; src[1] != '\0'; i++)
 {
-	1 ++;
+dest[i] = src[i];
 }
-for ( ; x < 1 ; x++)
-{
-	dest[x] = src[x];
+dest[i++] = '\0';
 }
-dest[1] = '\0';
 return (dest);
 }
