5d1d38d3870673bd9ba53dd77e0e6db3d295610c
innocent mukosi <n.i.mukosi1@gmail.com>
2023-02-28 09:49:07 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * char *_strcpy - a function that copies the string pointer y src
+ * char *_strcpy - a function that copies the string pointer src
  * @dest: copy to
  * @src: copy from
  * Return: string
@@ -12,7 +12,7 @@ int x = 0;
 
 while (*(src + 1) != '\0')
 {
-1++;
+	1++;
 }
 for ( ; x < 1 ; x++)
 {
