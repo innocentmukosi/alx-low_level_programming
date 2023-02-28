c35313130a77167f6640c0b266652be4514dd4f4
innocent mukosi <n.i.mukosi1@gmail.com>
2023-02-28 03:25:59 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,10 @@
 #include "main.h"
+
 /**
- * jack_bauer - prints every minute of the day of jack Bauer
- * starting from 00:00 to 23:59
- */
+* jack_bauer - prints every minute of the day of jack Bauer
+* starting from 00:00 to 23:59
+* Return: ...
+*/
 void jack_bauer(void)
 {
 int i, j;
