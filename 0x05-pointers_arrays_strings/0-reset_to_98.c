5b0b0b87500724ad32642918b5122e89ff5ad68c
innocent mukosi <n.i.mukosi1@gmail.com>
2023-02-28 05:53:47 -0800

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that take a pointer
+ * and updates the value it points to 98
+ * @n: input
+ * Return: n
+ */
+void reset_to98(int *n)
+{
+	*n = 98;
+}
