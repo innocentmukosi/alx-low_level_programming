18f227e37d2a6c76751fd3feb640e179221e95c4
innocent mukosi <n.i.mukosi1@gmail.com>
2023-03-01 06:09:19 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_string(char *dest, *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *a);
+char *cap_string(char *a);
+char *leet(char *a);
+int _putchar(char c);
+
+#endif
