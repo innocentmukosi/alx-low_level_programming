fcbd7451e23596700592f5095f72cfb6309ae689
innocent mukosi <n.i.mukosi1@gmail.com>
2023-03-02 08:12:27 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,25 +1,27 @@
 #include "main.h"
 
 /**
- * leet - encodes a string into 1337
- * @n: input
- * Return: the value of n
+ * *leet - encodes a string into 1337
+ * @str:character
+ * Return: char
  */
 
-char *leet(char *n)
+char *leet(char *str)
 {
+int *enc = str;
+char *letter = "aAeEoOtTlL";
+char num = "4433007711"
 int i, j;
-char s1[] = "aAeEoOtTlL";
-char s2[] = "4433007711"
 
-for (i = 0; n[i] != '\0'; i++)
+for (i = 0; str[i] != '\0'; i++)
 {
 for (j = 0; j < 10; j++)
 {
-if (n[i] == s1[j])
+if (str[i] == letter[j])
 {
-n[i] = s2[j};
+enc[i] = num[j};
+break
 }
 }
-return (n);
+return (enc);
 }
