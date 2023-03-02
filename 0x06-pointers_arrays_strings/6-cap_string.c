af9426edcc22166e620c60b89f1430e637b8836e
innocent mukosi <n.i.mukosi1@gmail.com>
2023-03-02 13:03:21 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,8 @@ while (str[i])
 {
 while (!(str[i] >= 'a' && str[i] <= 'z'))
 i++;
-if (str[i - 1] == '' ||
+if
+(str[i - 1] == ' ' ||
 (str[i - 1] == '\t' ||
 (str[i - 1] == '\n' ||
 (str[i - 1] == ',' ||
