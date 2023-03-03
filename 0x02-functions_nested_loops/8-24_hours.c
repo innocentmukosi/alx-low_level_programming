df6731b0b9f79d1257266b3fb300f87d7a738ad1
innocent mukosi <n.i.mukosi1@gmail.com>
2023-03-03 15:37:04 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,33 +8,18 @@
 */
 void jack_bauer(void)
 {
-int i, j;
+int hours, minute;
 
-for (i = 0; i < 24; i++)
+for (hours = 0; hour < 24; hour++)
 {
-for (j = 0; j < 60; j++)
+for (minute = 0; minute < 60; minute++)
 {
-if (i < 10)
-{
-_putchar('0');
-_putchar(i + '0');
-}
-else if (i >= 10)
-{
-_putchar((i / 10) + '0');
-_putchar((i % 10) + '0');
-}
-if (j < 10)
-{
+_putchar(hour / 10 + '0');
+_putchar((hour % 10 + '0');
 _putchar(':');
-_putchar('0');
-_putchar(j + '0');
-}
-else if (j > 10)
-{
-_putchar((j / 10) + '0');
-_putchar((j % 10) + '0');
-}
+_putchar(minute / 10 + '0');
+_putchar(minute % 10 + '0');
+_putchar('\n');
 }
 }
 }
