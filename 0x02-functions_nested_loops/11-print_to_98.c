83f4f1cad62aa58d540dfaf051d0cc6ea1c7e6c6
innocent mukosi <n.i.mukosi1@gmail.com>
2023-03-03 13:57:42 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,37 +9,22 @@
 
 void print_to_98(int n)
 {
+	int i;
+
 if (n <= 98)
-}
-for (; n <= 98; n++)
 {
-if (n == 98)
-i{
-printf("%d", n);
-printf("\n");
-break;
-}
-else
+for (i = n; i < 98; i++)
 {
-	printf("%d, ", n);
-}
+printf("%d, ", i);
 }
 }
 else
 {
-for (; n >= 98; n--)
+for (i = n; i > 98; i--)
 {
-if (n == 98)
-{
-printf("%d", n);
-printf("\n");
-break;
-}
-else
-{
-printf("%d, ", n);
+printf("%d, ", i);
 }
 }
-}
+printf("98\n");
 }
 
